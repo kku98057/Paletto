888698950595335291672be618c1a9f0a9add09c
김기태 <kku98057@naver.com>
2024-12-06 16:59:47 +0900

    깃헙 페이지 빌드용 브랜치 추가

--- src/router.tsx
+++ src/router.tsx
@@ -8,35 +8,38 @@ import PalettesPage from "./pages/PalettePage";
 import SavedColorsPage from "./pages/SavedColorsPage";
 import EditPalettePage from "./pages/EditPalettePage";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "/",
-        element: <SingleColorPage />,
-      },
-      {
-        path: "/harmony",
-        element: <HarmonyPage />,
-      },
-      {
-        path: "/palettes/new",
-        element: <NewPalettePage />,
-      },
-      {
-        path: "/palettes/:id/edit",
-        element: <EditPalettePage />,
-      },
-      {
-        path: "/palettes",
-        element: <PalettesPage />,
-      },
-      {
-        path: "/saved",
-        element: <SavedColorsPage />,
-      },
-    ],
-  },
-]);
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <SingleColorPage />,
+        },
+        {
+          path: "/harmony",
+          element: <HarmonyPage />,
+        },
+        {
+          path: "/palettes/new",
+          element: <NewPalettePage />,
+        },
+        {
+          path: "/palettes/:id/edit",
+          element: <EditPalettePage />,
+        },
+        {
+          path: "/palettes",
+          element: <PalettesPage />,
+        },
+        {
+          path: "/saved",
+          element: <SavedColorsPage />,
+        },
+      ],
+    },
+  ],
+  { basename: "/Paletto" }
+);
